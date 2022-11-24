71fcad9a5ae9ac0550ec7667b76239e7125a129b
Gandhar P <gandhar.pednekar15@gmail.com>
2022-11-24 23:07:46 +0530

    react app init

---
+++ cryptoapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Switch, Route, Link} from 'react-router-dom';
+import { Layout, Typography, Space} from 'antd';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Cryptoverse</h1>
+    </div>
+  );
+}
+
+export default App;
