4acbac0eeb57726bb664f22360fa3886e4bccea4
Gandhar P <gandhar.pednekar15@gmail.com>
2023-02-02 23:52:23 +0530

    homepage

--- cryptoapp/src/components/Cryptocurrencies.jsx
+++ cryptoapp/src/components/Cryptocurrencies.jsx
@@ -1,6 +1,44 @@
-const Cryptocurrencies = () => {
+import millify from "millify";
+import { Link } from "react-router-dom";
+import {Card, Row, Col, Input } from 'antd';
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { useState, useEffect } from "react";
+
+
+const Cryptocurrencies = ({ simplified }) => {
+    const count = simplified ? 10 : 100;
+    const {data: cryptosList, isFetching} = useGetCryptosQuery(count);
+    console.log(cryptosList)
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+    useEffect(() => {
+        if (!isFetching) {
+            setCryptos(cryptosList?.data?.coins)
+        }
+    }, [isFetching])
+
+    if(isFetching) return 'Loading ...';
+
     return (
-        <div>Cryptocurrencies</div>
+        <>
+            <div className="search-crypto">
+                <Input placeholder="Search Cryptocurrency" onChange={}></Input>
+            </div>
+            <Row gutter={[32, 32]} className="crypto-card-container">
+                {cryptos?.map(currency => (
+                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+                        <Link to={`/crypto/${currency.id}`}>
+                            <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl} />}
+                            hoverable>
+                                <p>Price: {millify(currency.price)}</p>
+                                <p>Market Cap: {millify(currency.marketCap)}</p>
+                                <p>Daily Change: {millify(currency.change)}%</p>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
---
+++ cryptoapp/src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '5b869af552msh994ba00d32ef8c3p17f46bjsn3fbd236f9bb4',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = "https://coinranking1.p.rapidapi.com"
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl: baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
